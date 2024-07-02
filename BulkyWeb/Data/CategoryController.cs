4701261520713b918a71c229ed8765415215a60c
Tolga Akif Gönen <akiftolgagonen@gmail.com>
2024-07-02 21:44:39 +0300

    Proje dosyası ekle.

---
+++ BulkyWeb/Data/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Data
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create2(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder can't match the name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if ((categoryFromDb == null))
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+             
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if ((categoryFromDb == null))
+            {
+                return NotFound();
+            }
+
+
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")] // aynı delete aksiyon metodunu arıyacağı için ActionName("Delete") kullandık.
+        public IActionResult DeletePOST(int? id) //Parametlerer aynı olacağı iççin DELETEPOST yaptık
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+           
+
+        }
+
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key] //Entity Framework kullanırken bir sınıfın (entity) birincil anahtarını (primary key) belirtmek için kullanılır
+              //Entity Framework, veritabanı işlemlerinde hangi sütunun birincil anahtar 
+              //olarak kullanılacağını anlamak için bu özniteliği kullanır.
+              // Birincil anahtar her kaydı benzersiz bir şekilde tanımlayan sütundur***.
+        public int Id { get; set; }
+        [Required] // Entity Framework ve ASP.NET MVC'de bir özelliğin boş geçilemez olduğunu belirtmek için kullanılır.
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
